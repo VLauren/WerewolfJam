68768a33706954824feea65a3265652f79eae199
ByteHunter <info@bytehunter.net>
2022-01-30 13:43:06 +0100

    Información de los controles

--- Assets/Scripts/UIMainMenu.cs
+++ Assets/Scripts/UIMainMenu.cs
@@ -6,14 +6,33 @@ using UnityEngine.SceneManagement;
 
 public class UIMainMenu : MonoBehaviour
 {
+    [SerializeField] GameObject mainMenuGroup;
+    [SerializeField] GameObject controlsInfoGroup;
+
+    bool showControlsInfo;
+
     void Start()
     {
+        showControlsInfo = false;
         Debug.Log("Started");
     }
 
     void OnAnyKey(InputValue value)
     {
         Debug.Log("Starting the Game");
+        if (!showControlsInfo) {
+            mainMenuGroup.SetActive(false);
+            controlsInfoGroup.SetActive(true);
+            showControlsInfo = true;
+        } else {
+            SceneManager.LoadScene(1);
+            // StartCoroutine(StartGame());
+        }
+    }
+
+    IEnumerator StartGame()
+    {
+        yield return new WaitForSeconds(3);
         SceneManager.LoadScene(1);
     }
 }
